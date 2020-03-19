a2c330770bdb68b9d36671dd8013df7ae8ac4b08
gokaybalci <cyber-trixx@msn.com>
2020-03-20 00:10:49 +0300

    First commit

---
+++ Cipher_start.py
@@ -0,0 +1,53 @@
+from datetime import date
+
+## Tarihler şifreleme için gerekeceğinden önemli bir adım.
+today = str(date.today())
+
+
+def print_menu():
+    print(30 * "-" , "DIARY MENU" , 30 * "-")
+    print("1. Create a diary entry")
+    print("2. Append a diary entry")
+    print("3. Read a diary entry")
+    print("4. Exit")
+    print(67 * "-")
+
+loop = True
+
+
+while loop:          ## Boolean değerimiz false olana kadar devam edecek.
+    print_menu()
+    choice = input("Enter your choice [1-4]: ")
+     
+    if choice == 1:     
+        print("Creating a new diary entry:")
+        diary = open("gunluk-" + today + ".md","w+")
+        loop=False
+        d_text = input("")
+        diary.write(d_text)
+        print("Diary with the date " + today + " has been written into the folder.")
+        loop=True
+    
+    elif choice == 2:
+        print("Appending an existing entry:")
+        diary = open("gunluk-" + today + ".md","a")
+        loop=False
+        dl_added_text = input("")
+        diary.write(dl_added_text)
+        print("Diary with the date " + today + " has been overwritten.")
+
+    elif choice == 3:
+    	print("You decided to read a diary entry.")
+
+    elif choice == 4:
+        print("You've decided to exit the program. Ba-bye.")
+        loop=False # Boolean değerimizi false olarak değiştirecek.
+    else:
+        # 1-4 sayı değeri arasında bir değer girilmezse, hata mesajı verecek
+        input("No valid option is selected. Press enter to try again.")
+
+	
+
+
+
+
